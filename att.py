d39065a2e9751d27bd1ca9645eaeb929e4a10ae3
priyanshuarora595 <57026241+priyanshuarora595@users.noreply.github.com>
2021-07-10 00:29:07 +0530

    Update att.py

--- att.py
+++ att.py
@@ -1,39 +1,72 @@
-from datetime import date
+from datetime import date,datetime
 import pandas as pd
-import tkinter as tk
-from tkinter import filedialog
-import os
+import os.path
+import time
+
+src_file_path = r"D:\vscode_projects\python\attendance\atd_list.csv"
+end_time=time.ctime(os.path.getctime(src_file_path))
+
+today_date=date.today().strftime("%d/%m/%Y")
+
+data = pd.read_csv(src_file_path,encoding='utf-16',delimiter='\t')
+
+data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+
+# joined_data=data.loc[data['User Action']=="Joined"]
+# left_data=data.loc[data['User Action']=="Left"]
+# # print(joined_data)
+# # print(left_data)
+
+# dic=data.to_dict()
+# print(dic)
+
+student_login_status={'Full Name':[],today_date:[],'temp':[],'state':[]}
 
 
+for i in data['Timestamp']:
+    
+    name= data.loc[data['Timestamp']==i,"Full Name"]
+    name = name.values[0]
+    action = (data.loc[data['Timestamp']==i,"User Action"]).values[0]
 
-
-
-def open_file():
-    global src_file_dir
-    global dest_file_dir
+    if name not in student_login_status['Full Name']:
+        (student_login_status['Full Name']).append(name)
+        (student_login_status[today_date]).append(0)
+        (student_login_status['temp']).append(i)
+        (student_login_status['state']).append('Joined')
+        # print(name,(student_login_status['temp']))
     
     
-    init_dir=os.getcwd()
-    filename=filedialog.askopenfile(initialdir=init_dir,title='choose file',filetypes=(('xlsx file','*.xlsx'),('csv files','*.csv'),('all files','*.*')))
-    src_file_dir=filename.name
-    src_file_dir
+    elif action=='Left':
+        ind = (student_login_status['Full Name']).index(name)
+        cur_time = (student_login_status['temp'])[ind]
+        if type(cur_time) != int:
+            # print(cur_time)
+            stay_time = (i-cur_time).seconds
+            (student_login_status[today_date])[ind]+=stay_time
+            (student_login_status['temp'])[ind] = i
+            (student_login_status['state'])[ind]="Left"
+            # print(name,student_login_status['temp'])
+            
+    elif (name in student_login_status['Full Name']) and (action=="Joined") :
+        
+        ind = (student_login_status['Full Name']).index(name)
+        (student_login_status['temp'])[ind]=i
+        (student_login_status['state'])[ind]='Joined'
+    # print((student_login_status))
+        
     
-root = tk.Tk()
-root.title('Attendnce Marker')
-
-open_src_file_label = tk.Label(root,text='source file directory :').pack()
-input_box_src=tk.Text(root,height = 3,width = 25).pack()
-open_source_file_button = tk.Button(root,text='Open source file',command=open_file).pack()
-
-print(src_file_dir)
-
-
-root.mainloop()
+for j in student_login_status['state']:
+    if j=='Joined':
+        ind=student_login_status['state'].index(j)
+        cur_time=(student_login_status['temp'])[ind]
+        end_time=pd.Timestamp(year = 2021,  month = 7, day = 7, 
+                  hour = 12, minute=55, second = 24)
+        stay_time=(end_time-cur_time).seconds
+        (student_login_status[today_date])[ind]+=stay_time
 
 
+out_data=pd.DataFrame(student_login_status)
 
+print(out_data)
 
-# data = pd.read_csv(r"D:\downloads\meetingAttendanceList (2).csv",encoding='utf-16',delimiter='\t')
-
-# # data.drop('User Action',inplace=True, axis=1)
-# print(data)
