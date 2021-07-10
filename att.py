3e202b968a0abd6a19b9f67c8580cca01355c585
priyanshuarora595 <57026241+priyanshuarora595@users.noreply.github.com>
2021-07-10 16:50:52 +0530

    Update att.py

--- att.py
+++ att.py
@@ -3,70 +3,78 @@ import pandas as pd
 import os.path
 import time
 
-src_file_path = r"D:\vscode_projects\python\attendance\atd_list.csv"
-end_time=time.ctime(os.path.getctime(src_file_path))
+src_file_path = r"D:\vscode_projects\python\attendance\meetingAttendanceList.csv"
 
 today_date=date.today().strftime("%d/%m/%Y")
 
+end_time=pd.Timestamp(year = 2020,  month = 10, day = 27, hour = 13, minute=00, second = 00)
 data = pd.read_csv(src_file_path,encoding='utf-16',delimiter='\t')
 
 data['Timestamp'] = pd.to_datetime(data['Timestamp'])
 
-# joined_data=data.loc[data['User Action']=="Joined"]
-# left_data=data.loc[data['User Action']=="Left"]
-# # print(joined_data)
-# # print(left_data)
-
-# dic=data.to_dict()
-# print(dic)
 
 student_login_status={'Full Name':[],today_date:[],'temp':[],'state':[]}
 
+start_time=0
 
-for i in data['Timestamp']:
-    
-    name= data.loc[data['Timestamp']==i,"Full Name"]
-    name = name.values[0]
-    action = (data.loc[data['Timestamp']==i,"User Action"]).values[0]
-
+for i in data.index:
+   
+    name= data['Full Name'][i]
+    action = data['User Action'][i]
+    time_stamp = data['Timestamp'][i]
+    if i==0:
+        start_time=time_stamp
+        
     if name not in student_login_status['Full Name']:
         (student_login_status['Full Name']).append(name)
         (student_login_status[today_date]).append(0)
-        (student_login_status['temp']).append(i)
+        (student_login_status['temp']).append(time_stamp)
         (student_login_status['state']).append('Joined')
-        # print(name,(student_login_status['temp']))
+        
     
     
-    elif action=='Left':
+    elif ("Left" in action):
+          
         ind = (student_login_status['Full Name']).index(name)
         cur_time = (student_login_status['temp'])[ind]
-        if type(cur_time) != int:
-            # print(cur_time)
-            stay_time = (i-cur_time).seconds
-            (student_login_status[today_date])[ind]+=stay_time
-            (student_login_status['temp'])[ind] = i
-            (student_login_status['state'])[ind]="Left"
-            # print(name,student_login_status['temp'])
-            
-    elif (name in student_login_status['Full Name']) and (action=="Joined") :
+        stay_time = (time_stamp-cur_time).seconds
+        (student_login_status[today_date])[ind]+=stay_time
+        (student_login_status['temp'])[ind] = time_stamp
+        (student_login_status['state'])[ind]="Left"
+   
+                        
+    elif (name in student_login_status['Full Name']) and ("Joined" in action) :
         
         ind = (student_login_status['Full Name']).index(name)
-        (student_login_status['temp'])[ind]=i
+        (student_login_status['temp'])[ind]=time_stamp
         (student_login_status['state'])[ind]='Joined'
-    # print((student_login_status))
-        
+   
     
-for j in student_login_status['state']:
-    if j=='Joined':
-        ind=student_login_status['state'].index(j)
+for j in student_login_status['Full Name']:
+    ind=(student_login_status['Full Name']).index(j)
+  
+    if (student_login_status['state'])[ind]=='Joined':
         cur_time=(student_login_status['temp'])[ind]
-        end_time=pd.Timestamp(year = 2021,  month = 7, day = 7, 
-                  hour = 12, minute=55, second = 24)
+        
         stay_time=(end_time-cur_time).seconds
         (student_login_status[today_date])[ind]+=stay_time
+        (student_login_status['state'])[ind]="Meeting over"
+      
+
+out_data=pd.DataFrame(student_login_status)
+
+
+out_data=out_data.drop(columns=['state','temp'])
+
+total_time=(end_time-start_time).seconds
+
 
 
 out_data=pd.DataFrame(student_login_status)
 
-print(out_data)
+out_data['status'] = ["P" if (((student_login_status[today_date])[j])*100)/total_time > 50.0 else "A" for j in range(len(student_login_status[today_date]))]
+        
 
+out_data=out_data.drop(columns=['state','temp'])
+print(out_data)
+out_data.to_csv(r"D:\vscode_projects\python\attendance\general.csv")
